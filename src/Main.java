74752e24ea04a7d940cd6f78d52cdd6c0df7eff8
Irini <learningnewproggram>
2022-06-22 16:12:37 +0300

    второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" Цикл " + i);
+        }
+
+        for (int i = 0; i > 10; i++ ) {
+            System.out.println("Цикл " + --i);
+        }
+
+        for (int b = 3; b < 31; b = b +7) {
+            System.out.println("Сегодня пятница, " + b + "число.Необходимо подготовить отчёт");
+        }
     }
 }
