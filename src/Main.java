e4f45f83196076e51024058f378524e9e961424b
Irini <learningnewproggram>
2022-06-22 16:38:51 +0300

    Третий коммит

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,15 @@ public class Main {
         }
 
         for (int b = 3; b < 31; b = b +7) {
-            System.out.println("Сегодня пятница, " + b + "число.Необходимо подготовить отчёт");
+            System.out.println("Сегодня пятница, " + b + " число.Необходимо подготовить отчёт");
+        }
+
+        int currentYear = 2022;
+        int cometPeriod = 79;
+        for (int i = 0; i < currentYear + cometPeriod; i += cometPeriod) {
+            if (i > currentYear - 200) {
+                System.out.println(i);
+            }
         }
     }
 }
